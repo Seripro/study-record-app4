6eb9e6ebadc03e00fb7fd224789e9bb72416c3b3
keigo <2364138keigo@gmail.com>
2026-06-13 20:45:44 +0900

    Recordモデルを追加し、学習記録のデータ構造を定義

---
+++ backend/app/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, DateTime, UniqueConstraint
+from sqlalchemy.sql import func
+from .database import Base
+
+class Record(Base):
+    __tablename__ = "study-record"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    time = Column(Integer, index=True)
